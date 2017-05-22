58aaf52fceb77e96d0294033476163146e4a24cd
Yann Cardon <yann.cardon@gmail.com>
2017-05-22 18:51:37 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Simple server that triggers system commands on predefined URLs
+// Used in conjunction with https://home-assistant.io/components/switch.rest/
+// To be demonized with http://pm2.keymetrics.io
+//
+// 2017.05.22 - Yann Cardon
+
+const http = require('http')
+const url = require('url')
+const exec = require('child_process').exec
+ 
+http.createServer( (req, res)=> {
+    switch (url.parse(req.url).pathname){
+        
+        // switch the macbook display on or off
+        case '/display':
+            req.body == 'ON' ? exec('caffeinate -u -t 1') : exec('pmset displaysleepnow')
+            break
+
+    }
+    res.end()
+}).listen(8182)
